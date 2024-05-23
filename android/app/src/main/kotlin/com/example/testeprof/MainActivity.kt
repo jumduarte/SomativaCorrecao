13c497ac56f107f7d2766591406ce7d6760d23d7
juju <132360412+jumduarte@users.noreply.github.com>
2024-05-23 11:55:44 -0300

    correção código

---
+++ android/app/src/main/kotlin/com/example/testeprof/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testeprof
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:testeprof/principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(), //
+));
